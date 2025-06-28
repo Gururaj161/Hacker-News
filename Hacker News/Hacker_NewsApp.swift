e2eb8957e82c0cfe510598878a6d16a63f82c7b3
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 23:27:31 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Gururaja M on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
